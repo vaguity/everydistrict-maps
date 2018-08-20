e7015b17dc3379d1a818f5d8811c6fb00c1259ba
Sean Connolly <sean.connolly@gmail.com>
2018-08-20 03:19:52 -0400

    Update everydistrict-maps-national-map-widget.php

--- everydistrict-maps-national-map-widget.php
+++ everydistrict-maps-national-map-widget.php
@@ -7,9 +7,20 @@ add_action( 'widgets_init', 'everydistrictm_load_national_map_widget' );
 
 class everydistrictm_national_map_widget extends WP_Widget {
 
-function __construct() {
-    parent::__construct(
-        'everydistrictm_national_map_widget',
-        __( 'EveryDistrict National Map Widget', 'everydistrict_widget_domain' ),
-        array( 'description' => __( 'Widget to display EveryDistrict national map on home page.', 'everydistrict_widget_domain' ), )
+    public function __construct() {
+        $widget_ops = array(
+            'classname' => 'everydistrictm_national_map_widget',
+            'description' => 'Widget to display EveryDistrict national map on home page.',
+        );
+        parent::__construct( 'everydistrictm_national_map_widget', 'EveryDistrict National Map Widget', $widget_ops );
+    }
+
+    public function widget( $args, $instance ) {
+        // outputs the content of the widget
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        // processes widget options to be saved
+    }
+
 }
