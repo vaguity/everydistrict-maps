23769c8ab07fc47587b4a21547ad90107554495f
dmorrison92 <drew.e.morrison@gmail.com>
2018-09-01 22:45:31 -0400

    Update everydistrict-maps.js

--- src/js/everydistrict-maps.js
+++ src/js/everydistrict-maps.js
@@ -5,6 +5,7 @@ $(window).on('load', function () {
 
     var MAP_NAMES = {
         'us': 'U.S.',
+        'al-house': 'Alabama House",
         'az-house': 'Arizona House',
         'az-senate': 'Arizona Senate',
         'ct-house': 'Connecticut House',
